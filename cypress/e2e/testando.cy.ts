5cc214eda5630750ed220b3613d6d25aee26988c
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-07-09 22:15:16 -0300

    Refactor: Improve code structure

--- cypress/e2e/testando.cy.ts
+++ cypress/e2e/testando.cy.ts
@@ -1,10 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:4200/#/home')
-    cy.contains("a", "Register now").click();
-    cy.get('[formcontrolname = "email"]').type("joseane@gmail.com");
-    cy.get('[formcontrolname = "fullName"]').type("joseaneGuedes");
-    cy.get('[formcontrolname = "password"]').type("joseane123");
-    cy.contains("button", "Register").click();
+describe('Página de cadastro', () => {
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('[data-test="register"]').click();
+    cy.get('input[data-test="email"]').type('lllll@lll.com');
+    cy.get('input[data-test="fullName"]').type('Lllll Lllll');
+    cy.get('input[data-test="registerUserName"]').type('lllllllll');
+    cy.get('input[data-test="registerPassword"]').type('LllllLllll');
+    cy.get('[data-test="btnRegister"]').click();
   })
 })
